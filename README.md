44b13071890ae69c9ebf2f4ca6fc6be5f69d4165
Ian Bridges <me@iannbridges.com>
2022-07-17 16:19:39 -0400

    updated tree name, added todo in file

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 A collection of utility scripts that I use for either writing, programming, or 
 doing mathematics. Each script has a multi-line comment at the beginning that 
-explains its functionality. 
+explains its functionality in greater detail. 
+
+- ptree.py: A script I use to generate my theorem trees so that they are more 
+  visually appealing.
--- ptree.py
+++ ptree.py
@@ -5,9 +5,15 @@ SYNOPSIS
     tree [-f file]
 FUNCTION
     given a text file with text indented by tabs, tree displays the file with the text organized into a tree like format. Useful for creating a visually appealing "theorem trees", etc. 
+TODO:
+    learn to read flags so that line spacing can be implemented
+    categorize tabs by level so that another flag can be implemented ignoring text of a certain depth
 """
 import sys
 text = sys.argv
+line_spacing = 0
+max_depth = 0 
+current_depth = 0 
 output = ""
 with open(str(text[1]), "r") as file:
     content = file.read()
